5f9548065ba1d3f2752045cd8588bed9388acb61
tomeschmusic <tomesch@gmail.com>
2021-01-27 17:45:33 +0000

    Version 6.0

--- Scripts/heybuddayguy.sh
+++ Scripts/heybuddayguy.sh
@@ -7,18 +7,23 @@
 # Purpose:                      Learning 
 
 # What is a variable? It is an object
+#some classmates used lspci
+network="ip a"
+network2="lspci"
+
+# Main
+$network
 
 # Declaration
 greeting="Hey Budday GUY!"
-
 # Call
 echo $greeting
-
+# And why not
 greeting="I'm not your Budday GUY, I'm your FWEND!"
 echo $greeting
 
 # Adding network fetch
 # Save retrieved info to a variable
 # Generates an output.txt file containing the network adapter information
-ip a > output.txt
-
+$network > output.txt
+$network2 > outputdbl.txt
