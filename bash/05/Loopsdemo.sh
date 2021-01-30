2f37e51fa6773a05b7cde588ace0eaa54a57697c
TomEschMusic <tomesch@gmail.com>
2021-01-30 07:37:28 +0000

    changes

---
+++ bash/05/Loopsdemo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script Name:                  Loop Demo 
+# Class Name:                   Ops 201
+# Author Name:                  Tom Esch
+# Date of latest revision:      1/29/2021
+# Purpose:                      Learning
+
+#Computers can keep track the runners on the looping track; can understand its start and stop
+
+# Declare variable
+names='Stan Kyle Cartman'
+
+# Declaration of functions
+
+
+#Main
+
+for name in $names
+do
+    echo $name
+done
+
+echo Complete #example of verbosity
+
+#End
---
+++ bash/05/democodegit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script:                       
+# Author:                       
+# Date of latest revision:      
+# Purpose:                      
+
+
+
+
+
+
+
+top
