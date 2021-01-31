6e657498790fc147980c128a9244ad6867fb96ed
TomEschMusic <tomesch@gmail.com>
2021-01-31 20:12:29 +0000

    updates

--- bash/05/loopdaddy.sh
+++ bash/05/loopdaddy.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
 # Script: loopdaddy.sh                       
-# Author: Tom Esch   Tom                    
-# Date of latest revision: 1/29/2021
+# Author: Tom Esch                       
+# Date of latest revision: 1/31/2021
 # Purpose:   Write a script that displays running processes, asks the user for a PID, then kills the process with that PID.
 # Use a loop in your script.  
 
-# Tasks
-# Write a script that displays running processes, asks the user for a PID, then kills the process with that PID.
-# Use a loop in your script.
-
 # Basic for loop
-names='Stan Kyle Cartman'
-for name in $names
+for answer in $ps -aux 
 do
-  echo $name
+    ps -aux
+
+    echo "Enter PID for process you wish to kill"
+
+    read answer
+
+    sudo kill $answer
 done
 
-echo All done
-
-# Declaration of variables
-
-# Main
-
-# All I came up with is something to do with TOP, but then I couldn't think through how to show that or something like that AND then get the prompt of asking a user to kill processes listed by entering PID. I also tried downloading something to run to kill safely, but do not seem to have luck finding server edition apps that install.
-
-# Websites
-
-# https://www.linux.com/training-tutorials/how-kill-process-command-line/
-# https://tecadmin.net/understanding-linux-top-command-results-uses/
-# https://www.cyberciti.biz/faq/how-to-end-task-with-pid-in-linux/ 
-# https://www.howtogeek.com/668986/how-to-use-the-linux-top-command-and-understand-its-output/
-#
-
+echo RIP, little process 
+echo The process has been killed 
 
 # End 
