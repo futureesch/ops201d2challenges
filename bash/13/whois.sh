baa9ac8aa7a27446d15489186941059c0dd4d42c
Tom Esch <77027955+tomeschmusic@users.noreply.github.com>
2021-02-10 18:01:56 -0800

    Create whois.sh

---
+++ bash/13/whois.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script: whois.sh
+# Author: Tom Esch
+# Date of latest revision: February 10th, 2021
+# Purpose: Create a bash script that asks a user to type a domain, then displays information about the typed domain. Operations that # must be used include:
+# whois
+# dig
+# host
+# nslookup
